0f0d9e68cff18a71b9f7a3191795506a8cd0ecc6
Weverton <wevertonmb.dev@gmail.com>
2026-06-26 10:55:43 -0300

    aula 001

---
+++ basics/aula001.py
@@ -0,0 +1,12 @@
+# Isso é um comentário
+
+"""
+Isto é um Docstring - usado para descrever o que o código faz, e pode ser acessado por meio da função help().
+"""
+
+# Comandos com print
+
+print("=" * 30) #operação com a string escrita.
+print("Curso de python - aula 001")
+print(10, 20, sep="*") # altera o separador padrão.
+print("comandos básicos do print", end="---") # altera o final de cada comando print, o padrão \n para pular linha.
