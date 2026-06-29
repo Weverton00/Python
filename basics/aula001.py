6e6731b00e3d07494baf121c78bd11cae1ec4b37
Weverton <wevertonmb.dev@gmail.com>
2026-06-29 10:51:55 -0300

    tipos primitivos

--- basics/aula001.py
+++ basics/aula001.py
@@ -10,3 +10,21 @@ print("=" * 30) #operação com a string escrita.
 print("Curso de python - aula 001")
 print(10, 20, sep="*") # altera o separador padrão.
 print("comandos básicos do print", end="---") # altera o final de cada comando print, o padrão \n para pular linha.
+
+# Tipos primitivos
+
+# Tipo Strings
+linguagem = "Python" #tipagem dinâmica
+area_de_atuacao: str = "automação" # tipagem forte
+
+#Tipo Int (número inteiro)
+idade = 25
+num: int = 45
+
+#Tipo Float (número com ponto flutuante)
+salario = 2499.99
+preco: float = 59.99
+
+#Tipo Boolean (true/False)
+is_over_18 = True
+is_open: bool = False
