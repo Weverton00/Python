cd84458d3c30c29d93f426ec55b1fb6b5a408b04
Weverton <wevertonmb.dev@gmail.com>
2026-06-30 10:26:12 -0300

    Operadores Aritméticos

--- basics/aula001.py
+++ basics/aula001.py
@@ -18,7 +18,7 @@ linguagem = "Python" #tipagem dinâmica
 area_de_atuacao: str = "automação" # tipagem forte
 
 #Tipo Int (número inteiro)
-idade = 25
+# idade = 25
 num: int = 45
 
 #Tipo Float (número com ponto flutuante)
@@ -28,3 +28,44 @@ preco: float = 59.99
 #Tipo Boolean (true/False)
 is_over_18 = True
 is_open: bool = False
+
+# ex 001
+
+nome: str = "Max"
+sobrenome: str = "Jr"
+idade: int = 10
+ano_nasc: int = 2026 - idade
+maior_de_idade: bool = idade >= 18
+altura_metros: float = 1.50
+
+print(f"Nome: {nome}")
+print(f"Sobrenome: {sobrenome}")
+print(f"Idade: {idade}")
+print(f"Ano de nascimento: {ano_nasc}")
+print(f"É maior de idade: {maior_de_idade}")
+print(f"Altura em metros: {altura_metros}")
+
+# ____________--------------_________________---------------
+
+# Operadores Aritméticos
+
+# adição +
+print(2 + 2)
+#subtração -
+print(10 - 5)
+# divisão /
+print(20 / 4)
+# multiplicação *
+print(5 * 4)
+# Resto da divisão ou módulo %
+print(21 % 4)
+# Divisão Inteira //
+print(30 // 2.5)
+# Exponenciação
+print(5 ** 3)
+
+# imc
+peso: float = 88.60
+altura: float = 1.86
+imc: float = peso / (altura * altura)
+print(f"Peso: {peso} Altura: {altura} IMC = {imc:.2f}")
