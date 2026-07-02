f989be6d2fe9b996203a44148cb2d172f52040bf
Weverton <wevertonmb.dev@gmail.com>
2026-07-02 10:59:07 -0300

    operadores lógicos - interpolação de strings

--- basics/aula002.py
+++ basics/aula002.py
@@ -19,3 +19,30 @@ elif idade >= 18:
   print("Você é maior de idade.")
 else:
   print("Digite um valor válido.")
+
+# Operadores Lógicos
+# and (e) or (ou) not (não)
+
+username: str = input("Digite o nome de usuário:")
+password: str = input("digite a senha:")
+
+correct_password: str = "abc123xyz456"
+correct_username: str = "john"
+
+if username == correct_username and password == correct_password:
+  print("Login realizado.")
+elif username == correct_username and password != correct_password:
+  print("Senha Incorreta.")
+else:
+  print("Tente novamente.")
+
+# in e not in - em iterações
+word: str = "Probabilidade"
+print("i" in word)
+
+# Interpolação de strings
+# s - string | i ou d - inteiro | f - float | x - hexadecimal
+
+
+print("%s custa %f.2f" % ("Playstation", 4600.4567))
+print("%d em hexadecimal é %04x" % (200, 200))
