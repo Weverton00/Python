cc458a889e42ef81656fcfb2417e986ec197e187
Weverton <wevertonmb.dev@gmail.com>
2026-07-03 10:25:48 -0300

    fatiamento de strings

--- basics/aula002.py
+++ basics/aula002.py
@@ -46,3 +46,24 @@ print("i" in word)
 
 print("%s custa %f.2f" % ("Playstation", 4600.4567))
 print("%d em hexadecimal é %04x" % (200, 200))
+
+# Fatiamento de Strings [início:fim:salto]
+new_word: str = "Paralelepipedo"
+print(new_word[0::2])
+print(new_word[7:])
+print(new_word[3:8])
+print(len(new_word))
+
+# ------------------
+name: str = input("Digite o seu nome: ")
+age: int = int(input("Digite sua idade: "))
+
+if name and age:
+  print(f"O seu nome é: {name}.")
+  print(f"O seu nome invertido é: {name[::-1]}")
+  print(f"O seu nome contém espaços? {" " in name}")
+  print(f"O seu nome tem {len(name)} letras.")
+  print(f"A primeira letra do seu nome é: {name[0]}")
+  print(f"A última letra do seu nome é: {name[-1]}")
+else:
+  print("Não deixe o campo 'name' vazio.")
