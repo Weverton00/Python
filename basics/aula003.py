87f3f9b3c4fb3cb82d794987b3070c40e03d2af8
Weverton <wevertonmb.dev@gmail.com>
2026-07-06 10:44:27 -0300

    exercícios simples

--- basics/aula003.py
+++ basics/aula003.py
@@ -8,3 +8,25 @@ try:
   print(f"O dobro do número {numero} é {num_int * 2}")
 except:
   print("Não é um número.")
+
+# Ex --- 01 
+
+number: int = int(input("Digite um número: "))
+
+if number % 2 == 0:
+  print(f"O número {number} é par.")
+elif number % 2 == 1:
+  print(f"O número {number} é ímpar.")
+else:
+  print("Insira um valor válido.")
+
+# Ex --- 02
+
+name: str = input("Qual é o seu nome? ")
+
+if len(name) <= 4:
+  print("O seu nome é curto.")
+elif len(name) > 4 and len(name) < 6:
+  print("O seu nome é normal.")
+else:
+  print("O seu nome é muito longo.")
