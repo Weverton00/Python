9e6050b0d76a3c915ff75f4c4888804bc1ae23c8
Weverton <wevertonmb.dev@gmail.com>
2026-07-08 10:50:37 -0300

    calc com while

--- basics/aula004.py
+++ basics/aula004.py
@@ -2,20 +2,43 @@
 # While
 # continue / break
 
-contador: int = 20
+# contador: int = 20
 
-while contador >= 1:
-  contador -= 1
+# while contador >= 1:
+#   contador -= 1
 
-  if contador == 10:
-    continue
+#   if contador == 10:
+#     continue
 
-  print(f"{contador}")
+#   print(f"{contador}")
 
 # -----------
-while True:
-  nome: str = input("Qual é o seu nome: ")
-  print(nome)
+# while True:
+#   nome: str = input("Qual é o seu nome: ")
+#   print(nome)
 
-  if nome == "sair":
+#   if nome == "sair":
+#     break
+
+# Calculadora com while
+
+while True:
+  first_num: int = int(input("Digite um número: "))
+  second_num: int = int(input("Digite outro número: "))
+  operador: str = input("Digite a operação desejada: ( + adição) ( - subtração) ( * multiplicação) ( / divisão)")
+  
+  if operador == "+":
+    print(f"resultado: {first_num + second_num}")
+  elif operador == "-":
+    print(f"Resultado: {first_num - second_num}")
+  elif operador == "*":
+    print(f"Resultado: {first_num * second_num}")
+  elif operador == "/":
+    print(f"Resultado: {first_num / second_num}")
+  else:
+    print("Insira um operador válido")
+
+  sair = input("Deseja sair? [sim]").lower().startswith("s")
+
+  if sair is True:
     break
