a0aff21a932888782b9800e2fdf2d3c9d817195b
Weverton <wevertonmb.dev@gmail.com>
2026-07-15 11:13:55 -0300

    métodos split e join

---
+++ basics/aula007.py
@@ -0,0 +1,8 @@
+# split / join - métodos de strings / listas
+
+msg: str = "Olá! Seja bem-vindo ao grupo."
+nomes: list = ["Max", "Bella", "Louis"]
+
+msg_tokenizada: list = msg.split()
+
+print(msg_tokenizada)
