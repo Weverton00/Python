4e297093d62a9e7fbcc4ef89dfdc62460376b90d
Weverton <wevertonmb.dev@gmail.com>
2026-07-22 10:54:53 -0300

    Funções e escopos

--- advanced/functions.py
+++ advanced/functions.py
@@ -12,3 +12,27 @@ def soma(a: int, b: int):
   print(a + b)
 
 soma(5, 10)
+
+# Escopos de funções Global / Local
+
+def local_escope():
+  x: str = "valor local"
+  print(x)
+
+#print(x) X - não está definida globalmente, resultará em um erro.
+country: str = ""
+
+def global_escope():
+  global country # acessando a variável global
+  country: str = "brasil"
+  print(country)
+
+print(country)
+
+#Retorno das funções
+
+def mult(a: int, b: int):
+  return a * b
+
+multiplicao = mult(5,10)
+#Passar múltiplos parâmetros def mult(*args)
